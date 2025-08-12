fbb5045c942b1510fb346bec74f473c75d342304
PALAK VARSHNEY <palakvarshney1712@gmail.com>
2025-08-12 11:58:20 +0530

    Update Q9.java

--- Q9.java
+++ Q9.java
@@ -1,25 +1,20 @@
-import java.util.Scanner;
-
-public class Q9 {
-
+import java.util.*;
+public class Main {
     public static void main(String[] args) {
-    
-        Scanner input = new Scanner(System.in);
+                Scanner sc= new Scanner(System.in);
 
-        
+
         System.out.print("Enter the dividend (first integer): ");
-        int dividend = input.nextInt(); 
+        int dividend = sc.nextInt(); 
 
         System.out.print("Enter the divisor (second integer): ");
-        int divisor = input.nextInt(); 
-        if (divisor == 0) {
-            System.out.println("Error: Divisor cannot be zero.");
-        } else {
-            
-            int quotient = dividend / divisor;                  int remainder = dividend % divisor;
-            System.out.println("Quotient: " + quotient);
-            System.out.println("Remainder: " + remainder);
+        int divisor = sc.nextInt(); 
+        
+
+        int quotient = dividend/ divisor;                  
+        int remainder = dividend % divisor;
+   System.out.println("Quotient: "+quotient);
+   System.out.println("Remainder:"+remainder);
         }
-        input.close();
+        
     }
-}
